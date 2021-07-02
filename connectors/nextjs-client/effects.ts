ac8fb81b3bf219806d438adf174a26446ac32a59
Rholang <rholang@protonmail.com>
2021-07-02 14:40:18 +0200

    api test

--- connectors/nextjs-client/effects.ts
+++ connectors/nextjs-client/effects.ts
@@ -7,9 +7,8 @@ export const nextjsExploreDeploy = async ({ node, code }) => {
     //formData.append('code', code);
 
     const resultRaw = await fetch('/api/explore', {
-      method: 'GET',
-      //body: JSON.stringify(formValues),
-      cache: 'default',
+      method: 'POST',
+      body: JSON.stringify(formValues),
     });
     // const res = await sendToApi('POST', `/api/explore`, body);
 
