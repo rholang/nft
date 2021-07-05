90201f8a49739c2400b5d60fc002a72fda576238
Rholang <rholang@protonmail.com>
2021-07-05 22:25:08 +0200

    test

--- connectors/nextjs-client/effects.ts
+++ connectors/nextjs-client/effects.ts
@@ -4,6 +4,19 @@ export const nextjsExploreDeploy = async ({ node, code }): Promise<Status> => {
   try {
     const formattedCode = code.replace(/(\r\n|\n|\r)/gm, '');
 
+    const ab = await axios.get<void>(
+      'https://blog.cloudflare.com/content/images/2015/08/cache-hit.png',
+      {
+        method: 'GET',
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Content-Type': 'application/json',
+        },
+      }
+    );
+
+    console.log(ab.toString());
+
     const res = await axios.get<Status>('/api/explore', {
       params: {
         node: node,
