da393b809f117667eb4a66415d8b93708993c8c1
Rholang <rholang@protonmail.com>
2021-07-07 15:40:10 +0200

    bugfix

--- connectors/cf-client/effects.ts
+++ connectors/cf-client/effects.ts
@@ -2,22 +2,10 @@ import axios, { AxiosResponse } from 'axios';
 import { Status } from 'connectors/rnode-client';
 export const cfExploreDeploy = async ({ node, code }): Promise<Status> => {
   try {
-    const formattedCode = code.replace(/(\r\n|\n|\r)/gm, '');
-
-    const data = {
-      net: 'testnet',
-      code: formattedCode,
-    };
-
-    const options = {
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    };
     const res = await axios.get('/explore', {
       params: {
-        net: node,
-        code: formattedCode,
+        net: 'testnet',
+        code: code,
       },
     });
 
