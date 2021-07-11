7e639cc0de1bbaf57d3b092e9066547f1d15bd0c
Rholang <rholang@protonmail.com>
2021-07-12 00:39:36 +0200

    bugifx plugin

--- packages/vite-plugin-rholang/src/index.ts
+++ packages/vite-plugin-rholang/src/index.ts
@@ -9,23 +9,23 @@ export interface PluginOptions {
 
 const matchArgs = async (quotedCode: string) => {
     /* matchedArgs = all rho:arg1:entryUri */
-    const matchedArgs = quotedCode.match(/rho:arg(\d+:\w+)`/);
+    const matchedArgs = [...quotedCode.matchAll(/rho:arg(\d+:\w+)`/g)];
     if (matchedArgs) {
         /* mArgs = all position and name of args */
         const mArgs = matchedArgs
             .map((argEl) => {
-                const position = argEl.match(/arg(\d+)/);
-                const name = argEl.match(/arg\d+:(\w+)\u0060/);
+                const position = argEl[0].match(/arg(\d+)/);
+                const name = argEl[0].match(/arg\d+:(\w+)\u0060/);
 
                 if (position && name) {
-                    console.log(name[1]);
-                    const p = position;
-                    const n = name;
+                    const p = position[1];
+                    const n = name[1];
                     return { position: p, name: n };
                 }
                 return { position: "", name: "" };
             })
             .filter((value) => value.name !== "" || value.position !== "");
+
         if (mArgs) {
             const sortedmArgs = mArgs.sort((a, b) => (a.position > b.position ? 1 : -1));
 
@@ -34,7 +34,7 @@ const matchArgs = async (quotedCode: string) => {
                 return acc.replaceAll(`rho:arg${currVal.position}:${currVal.name}`, templLit);
             }, quotedCode);
 
-            const argumentsCode = sortedmArgs.map((el) => el.name);
+            const argumentsCode = sortedmArgs.map((el) => el.name).flat();
             console.log(argumentsCode);
 
             return { argumentsCode, argsReplacedCode };
@@ -57,9 +57,7 @@ const transform = async (code: string, id: string) => {
         const doubleQuotes = backTicks.replace(/\u0022/g, "\u0060");
         const replacedBTCode = doubleQuotes.replace(/\u005C\u005C\u0060/g, "\u005C\u0060");
         console.log(replacedBTCode);
-        const parsedCode = `export const ${fileName} = ${argumentsCode} => ${replacedBTCode}`;
-        console.log("parsedCode");
-        console.log(parsedCode);
+        const parsedCode = `export const ${fileName} = (${argumentsCode}) => ${replacedBTCode}`;
         return parsedCode;
     }
     return ``;
