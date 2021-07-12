3654ff3bfb77849e49f2df42c105c7a19ba1f503
Rholang <rholang@protonmail.com>
2021-07-12 18:33:07 +0200

    effect

--- packages/app/connectors/rnode-client/effects.ts
+++ packages/app/connectors/rnode-client/effects.ts
@@ -1,57 +1,56 @@
-import { NodeUrls, Status, DeployDetailArgs } from './types';
-import { domain, Store as S } from './model';
 import {
-  makeRNodeWeb,
-  rhoExprToJson,
-  RevAccount,
-} from 'connectors/rnode-http-js';
-import { getNodeUrls } from './network';
-import { attach } from 'effector';
+    makeRNodeWeb,
+    rhoExprToJson,
+    RevAccount,
+    ethereumAddress,
+    createRevAccount,
+    RevAddress,
+} from 'connectors/rnode-http-js'
+import { attach } from 'effector'
 import {
-  ExploreDeployArgs,
-  ExploreDeployEff,
-  DeployArgs,
-  DeployEff,
-  RNodeEff,
-} from 'connectors/rnode-client/types';
-import {
-  ethereumAddress,
-  createRevAccount,
-  RevAddress,
-} from 'connectors/rnode-http-js';
-import * as R from 'ramda';
-import { cfExploreDeploy } from 'connectors/cf-client';
+    ExploreDeployArgs,
+    ExploreDeployEff,
+    DeployArgs,
+    DeployEff,
+    RNodeEff,
+} from 'connectors/rnode-client/types'
+
+import * as R from 'ramda'
+import { cfExploreDeploy } from 'connectors/cf-client'
+import { getNodeUrls } from './network'
+import { domain, Store as S } from './model'
+import { NodeUrls, Status, DeployDetailArgs } from './types'
 
 const exploreDeploy = ({ rnodeHttp, node }: ExploreDeployEff) =>
-  async function ({ code }: ExploreDeployArgs): Promise<Status> {
-    const {
-      expr: [e],
-    } = await rnodeHttp(node.httpUrl, 'explore-deploy', code);
-    const dataBal = e as string;
-    //console.log(dataBal);
-    const dataError = (e && e.ExprString && e.ExprString.data) || '';
-    return { success: dataError, message: dataBal };
-  };
+    async function ({ code }: ExploreDeployArgs): Promise<Status> {
+        const {
+            expr: [e],
+        } = await rnodeHttp(node.httpUrl, 'explore-deploy', code)
+        const dataBal = e as string
+        // console.log(dataBal);
+        const dataError = (e && e.ExprString && e.ExprString.data) || ''
+        return { success: dataError, message: dataBal }
+    }
 
 const deploy = (effects: DeployEff) =>
-  async function ({ code, account, phloLimit }: DeployArgs): Promise<Status> {
-    const { node, sendDeploy, getDataForDeploy } = effects;
-    console.log(account);
-    const phloLimitNum = R.isNil(phloLimit) ? phloLimit : parseInt(phloLimit);
-    const { signature } = await sendDeploy(node, account, code, phloLimitNum);
+    async function ({ code, account, phloLimit }: DeployArgs): Promise<Status> {
+        const { node, sendDeploy, getDataForDeploy } = effects
+        console.log(account)
+        const phloLimitNum = R.isNil(phloLimit) ? phloLimit : parseInt(phloLimit)
+        const { signature } = await sendDeploy(node, account, code, phloLimitNum)
 
-    // Progress dots
-    const mkProgress = (i: number) => () => {
-      i = i > 60 ? 0 : i + 3;
-      return `Checking result ${R.repeat('.', i).join('')}`;
-    };
-    const progressStep = mkProgress(0);
-    const updateProgress = () => true;
-    updateProgress();
+        // Progress dots
+        const mkProgress = (i: number) => () => {
+            i = i > 60 ? 0 : i + 3
+            return `Checking result ${R.repeat('.', i).join('')}`
+        }
+        const progressStep = mkProgress(0)
+        const updateProgress = () => true
+        updateProgress()
 
-    // Try to get result from next proposed block
+        // Try to get result from next proposed block
 
-    /*const getData = async () => {
+        /* const getData = async () => {
       const { data, cost } = await getDataForDeploy(
         node,
         signature,
@@ -75,112 +74,110 @@ const deploy = (effects: DeployEff) =>
 
     if (!succ) throw Error(`Deploy error: ${message}. // cost: ${costTxt}`);
 
-    const success = succ.toString();*/
-    const message = signature;
-    return { success: '', message: message };
-  };
+    const success = succ.toString(); */
+        const message = signature
+        return { success: '', message }
+    }
 
 export const getMetamaskAccount = async () => {
-  const ethAddr = await ethereumAddress();
-  const revAddress: RevAddress = createRevAccount(ethAddr);
-  const revAccountAddress = { ethAddr: ethAddr, ...revAddress };
-  const revAccountName = { name: 'revWallet' };
-  const revAccount: RevAccount = { ...revAccountAddress, ...revAccountName };
-  return revAccount;
-};
+    const ethAddr = await ethereumAddress()
+    const revAddress: RevAddress = createRevAccount(ethAddr)
+    const revAccountAddress = { ethAddr, ...revAddress }
+    const revAccountName = { name: 'revWallet' }
+    const revAccount: RevAccount = { ...revAccountAddress, ...revAccountName }
+    return revAccount
+}
 
 export const createRnodeService = (node): RNodeEff => {
-  const rnodeWeb = makeRNodeWeb({ now: Date.now });
+    const rnodeWeb = makeRNodeWeb({ now: Date.now })
 
-  const { rnodeHttp, sendDeploy, getDataForDeploy } = rnodeWeb;
+    const { rnodeHttp, sendDeploy, getDataForDeploy } = rnodeWeb
 
-  // App actions to process communication with RNode
-  return {
-    exploreDeploy: exploreDeploy({ rnodeHttp, node }),
-    deploy: deploy({ node, sendDeploy, getDataForDeploy }),
-  };
-};
+    // App actions to process communication with RNode
+    return {
+        exploreDeploy: exploreDeploy({ rnodeHttp, node }),
+        deploy: deploy({ node, sendDeploy, getDataForDeploy }),
+    }
+}
 
 export const effectsRouter = async ({ fn, params, node }) => {
-  const { client, code, account, phloLimit } = params;
+    const { client, code, account, phloLimit } = params
 
-  const { exploreDeploy, deploy } = createRnodeService(node);
+    const { exploreDeploy, deploy } = createRnodeService(node)
 
-  switch (client) {
-    case 'cf':
-      {
-        switch (fn) {
-          case 'exploreDeploy': {
-            const data = await cfExploreDeploy({ node, code });
-            return data;
-          }
+    switch (client) {
+        case 'cf': {
+            switch (fn) {
+                case 'exploreDeploy': {
+                    const data = await cfExploreDeploy({ node, code })
+                    return data
+                }
+            }
         }
-      }
-      break;
 
-    case 'rnode':
-      {
-        switch (fn) {
-          case 'exploreDeploy': {
-            const data = await exploreDeploy({ code });
-            return data;
-          }
+        case 'rnode': {
+            switch (fn) {
+                case 'exploreDeploy': {
+                    const data = await exploreDeploy({ code })
+                    return data
+                }
 
-          case 'deploy': {
-            const data = await deploy({ code, account, phloLimit });
-            return data;
-          }
+                case 'deploy': {
+                    const data = await deploy({ code, account, phloLimit })
+                    return data
+                }
+            }
         }
-      }
-      break;
-  }
-};
+        default:
+            return {}
+            break
+    }
+}
 
-const exploreDeployFxOrg = domain.effect<
-  { fn: string; params: any; node: NodeUrls },
-  Status
->(effectsRouter);
+const exploreDeployFxOrg = domain.effect<{ fn: string; params: any; node: NodeUrls }, Status>(
+    effectsRouter
+)
 
 const deployFxOrg = domain.effect<
-  {
-    fn: string;
-    params: DeployArgs;
-    node: NodeUrls;
-  },
-  Status
->(effectsRouter);
+    {
+        fn: string
+        params: DeployArgs
+        node: NodeUrls
+    },
+    Status
+>(effectsRouter)
 
-const addWalletFx = domain.effect(getMetamaskAccount);
+const addWalletFx = domain.effect(getMetamaskAccount)
 
 const exploreDeployFx = attach({
-  effect: exploreDeployFxOrg,
-  source: S.$rnodeStore,
-  mapParams: (paramsR, data) => {
-    //console.log('Created effect called with', params, 'and data', data);
-    const node = getNodeUrls(data.readNode);
-    return { fn: 'exploreDeploy', params: paramsR, node };
-  },
-});
+    effect: exploreDeployFxOrg,
+    source: S.$rnodeStore,
+    mapParams: (paramsR, data) => {
+        // console.log('Created effect called with', params, 'and data', data);
+        const node = getNodeUrls(data.readNode)
+        return { fn: 'exploreDeploy', params: paramsR, node }
+    },
+})
 
 const deployFx = attach({
-  effect: deployFxOrg,
-  source: S.$rnodeStore,
-  mapParams: (params: DeployArgs, data) => {
-    //console.log('Created effect called with', params, 'and data', data);
-    const node = getNodeUrls(data.valNode);
-    const account = data.walletSelected;
-    const paramsR = {
-      client: params.client,
-      code: params.code,
-      account: account,
-      phloLimit: params.phloLimit,
-    };
-    return { fn: 'deploy', params: paramsR, node };
-  },
-});
+    effect: deployFxOrg,
+    source: S.$rnodeStore,
+    mapParams: (params: DeployArgs, data) => {
+        // console.log('Created effect called with', params, 'and data', data);
+        const node = getNodeUrls(data.valNode)
+        const account = data.walletSelected
+        const paramsR = {
+            client: params.client,
+            code: params.code,
+            account,
+            phloLimit: params.phloLimit,
+        }
+        return { fn: 'deploy', params: paramsR, node }
+    },
+})
 
 export const Effects = {
-  deployFx,
-  exploreDeployFx,
-  addWalletFx,
-};
+    deployFx,
+    exploreDeployFx,
+    addWalletFx,
+}
