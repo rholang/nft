1ec233b669c5c59f0337c1317f3b3a3509bf44e5
Rholang <rholang@protonmail.com>
2021-07-19 11:54:30 +0200

    bugfix

--- packages/smart-contracts/vite.config.ts
+++ packages/smart-contracts/vite.config.ts
@@ -34,7 +34,7 @@ export default defineConfig({
       patterns: [
         {
           matchTokens: ["// Start_Exports", "// End_Exports"],
-          path: "src/rholang/nft2/**/*.rho",
+          path: "src/rholang/nft/**/*.rho",
         },
       ],
     }),
